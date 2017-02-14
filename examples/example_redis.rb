76c220154aaa09a3c604396088e50c7be0d7d5fe
Anthony Ross <anthony.ross@validic.com>
2017-02-14 10:34:09 -0500

    Add examples for memory and redis

---
+++ examples/example_redis.rb
@@ -0,0 +1,33 @@
+require 'circuit_breaker'
+require 'logger'
+require 'redis'
+require 'pry'
+
+@logger = Logger.new(STDOUT)
+
+handles = ["joe", "jane", "mary", "steve"]
+
+def get_tweets(twitter_handle, _num)
+  http_result = ["Success!", "Fail"].sample
+  raise RuntimeError.new("Failed to fetch tweets for #{twitter_handle}") if http_result == "Fail"
+  @logger.info "#{http_result} getting tweets for #{twitter_handle}"
+end
+
+redis = Redis.new
+
+breaker = CircuitBreaker.new do |cb|
+  cb.circuit = -> (twitter_handle, num) { get_tweets(twitter_handle, num) }
+  cb.adapter = :redis
+  cb.adapter_client = redis
+  cb.adapter_namespace = "get_tweets"
+  cb.failure_limit = 2
+  cb.reset_timeout = 5
+end
+
+handles.each_with_index do |handle, i|
+  begin
+    breaker.call(handle, i)
+  rescue CircuitBreaker::Open
+    sleep breaker.reset_timeout
+  end
+end
